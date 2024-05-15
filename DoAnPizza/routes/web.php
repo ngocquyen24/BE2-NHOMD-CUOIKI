740b358477e3f07b8833fa2a51af3b4eae20c5ce
Truongthithuyhoa <21211tt4610@mail.tdc.edu.vn>
2024-05-15 08:26:34 +0700

    DoAn

---
+++ DoAnPizza/routes/web.php
@@ -0,0 +1,38 @@
+<?php
+namespace Illuminate\Session\Middleware;
+
+use App\Http\Controllers\Admin\MainController;
+use Illuminate\Support\Facades\Route;
+use \App\Http\Controllers\Admin\Users\LoginController;
+
+
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('admin/users/login',[LoginController::class, 'index'])->name('login');
+Route::post('admin/users/login/store',[LoginController::class, 'store']);
+
+// Route::middleware(['auth'])->group(function () {
+
+// });
+
+//Route::get('admin/main',[MainController::class,'index'])->name('admin')->middleware('auth');
+Route::get('admin/main', function () {
+    Route::get('admin/main',[MainController::class,'index'])->name('admin');
+})->middleware('auth');
+
+
