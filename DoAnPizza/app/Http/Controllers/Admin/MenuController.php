9fd6850b96dd6e047630b04e23847ad164602c5d
Thanhdat328 <92036923+Thanhdat328@users.noreply.github.com>
2024-05-16 10:07:04 +0700

    delete

---
+++ DoAnPizza/app/Http/Controllers/Admin/MenuController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Menu\CreateFormRequest;
+use App\Http\Services\Menu\MenuService;
+use App\Models\Menu;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class MenuController extends Controller
+{
+    protected $menuService;
+
+    public function __construct(MenuService $menuService)
+    {
+        $this->menuService = $menuService;
+    }
+    public function create(){
+       return view('admin.menu.add' ,[
+           'title'=> 'them danh muc ',
+           'menus' => $this->menuService->getParent()
+       ]);
+    }
+    public function store(CreateFormRequest $request)
+    {
+        $this->menuService->create($request);
+        return redirect()->back();
+
+
+    }
+    public function index()
+    {
+        return view('admin.menu.list', [
+            'title'=>'danh sách danh mục mới nhất',
+            'menus'=>$this->menuService->getAll()
+
+        ]);
+    }
+    public function destroy(Request $request): JsonResponse
+    {
+        $result = $this->menuService->destroy($request);
+        if ($result) {
+            return response()->json([
+                'error' => false,
+                'message' => 'Xóa thành công danh mục'
+            ]);
+        }
+
+        return response()->json([
+            'error' => true
+        ]);
+    }
+
+}
